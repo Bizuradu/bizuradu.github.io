5c9efbb560e8258e4a5636700a8cc6cd5da98dd8
Bezout <teste@tested.com>
2025-09-29 16:10:53 -0400

    Conserto da última entrada do rank quando menor que 10 nomes.

--- CP10/cp10-0.4.0.service.worker.js
+++ CP10/cp10-0.4.0.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1759183010|446391486';
+const CACHE_VERSION = '1759176287|650815601';
 /** @type {string} */
 const CACHE_PREFIX = 'sml_CP10-v0.4-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
